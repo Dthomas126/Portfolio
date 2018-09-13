8a51677e83ead18cbc34c9e7730fe1dd22377f0b
Devin <dthom94@gmail.com>
2018-09-13 09:16:10 -0500

    Add main site page

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="style/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header role="banner" class="header">
+        <div class="padding-15">
+        <div class="logo ">
+        <h1><span class="wht active">WEB</span> DEVELOPER</h1>
+        <p>Devin Thomas</p>
+    </div>
+        <nav role="navigation" class="nav ">
+            <ul class="nav-list">
+                <li class="nav-item active">Home</li>
+                <li class="nav-item">About</li>
+                <li class="nav-item">Contact</li>
+            </ul>
+        </nav>
+    </div>
+    </header>
+    <main role="main" id="main">
+        <article role="article" id="article">
+            <section class="section">
+                    
+                <header>
+                    <h2>Technologies</h2>
+                    <p>Tech sub heading</p>
+                </header>
+
+                <section>
+
+                </section>
+
+        </section>
+        <section class="section">
+                <header>
+                        <h2>Projects</h2>
+                        <p>Project sub heading</p>
+                    </header>
+    
+                    <section>
+                        
+                    </section>
+        </section>
+
+
+        </article>
+        <footer class="footer" id="footer">
+            <h3>Footer Title</h3>
+            <ul>
+                <li>item</li>
+                <li>item</li>
+                <li>item</li>
+            </ul>
+        </footer>
+    </main>
+</body>
+</html>
