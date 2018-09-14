96dc2d425db144b4b0d8c84b648ec8e74e4a6e67
Devin <dthom94@gmail.com>
2018-09-14 03:03:11 -0500

    Reverted navbar to transparent

---
+++ inc/footer.inc.php
@@ -0,0 +1,12 @@
+<footer class="footer" id="footer">
+            <h3>Footer Title</h3>
+            <ul>
+                <li>item</li>
+                <li>item</li>
+                <li>item</li>
+            </ul>
+          
+        </footer>
+    </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style/style.css">
+<?php
+    include './inc/header.inc.php';
 
-    <title>Document</title>
-</head>
-<body>
-    <header role="banner" class="header">
-        <div class="contain">
-        <div class="logo ">
-        <h1><span class="wht active">WEB</span> DEVELOPER</h1>
-        <p>Devin Thomas</p>
-    </div>
-        <nav role="navigation" class="nav ">
-            <ul class="nav-list">
-                <li class="nav-item active"><a href="#">Home</a></li>
-                <li class="nav-item"><a href="#">About</a></li>
-                <li class="nav-item"><a href="#">Contact</a></li>
-                <li class="nav-item"><a href="#">Gallery</a></li>
-            </ul>
-        </nav>
-    </div>
-    </header>
+
+?>
     <main role="main" id="main">
         <article role="article" id="article">
             <section class="section">
@@ -87,15 +61,8 @@
 
 
         </article>
-        <footer class="footer" id="footer">
-            <h3>Footer Title</h3>
-            <ul>
-                <li>item</li>
-                <li>item</li>
-                <li>item</li>
-            </ul>
-          
-        </footer>
-    </main>
-</body>
-</html>
+        <?php
+    include './inc/footer.inc.php';
+
+
+?>
