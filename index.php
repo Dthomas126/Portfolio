f3133471eba39c48d76b11436fcea5f0f568e777
Devin <dthom94@gmail.com>
2018-09-14 02:55:55 -0500

    Update tech and project with photos

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style/style.css">
 
     <title>Document</title>
 </head>
 <body>
     <header role="banner" class="header">
-        <div class="padding-15">
+        <div class="contain">
         <div class="logo ">
         <h1><span class="wht active">WEB</span> DEVELOPER</h1>
         <p>Devin Thomas</p>
@@ -31,34 +34,54 @@
             <section class="section">
                     
                 <header>
-                    <h2>Technologies</h2>
-                    <p>Tech sub heading</p>
+                    <h2>Languages</h2>
+                    <p>Programming languages level of expirience.</p>
 
 
                 </header>
                     <ul class="tech-list">
-                        <li class="tech-item">HTML 5</li>
-                        <li class="tech-item">CSS3</li>
-                        <li class="tech-item">JQuery</li>
-                        <li class="tech-item">PHP</li>
-                        <li class="tech-item">Java</li>
-                        <li class="tech-item">SQL</li>
-                        <li class="tech-item">JavaScript</li>
-                        <li class="tech-item">VueJS</li>
+                        <li class="tech-item">HTML 5 <div class="progress">
+  <div class="progress-bar bg-dark" style="width:60%"></div>
+</div></li>
+                        <li class="tech-item">CSS3 <div class="progress">
+  <div class="progress-bar bg-dark" style="width:60%"></div>
+</div></li>
+                        <li class="tech-item">JQuery <div class="progress">
+  <div class="progress-bar bg-dark" style="width:35%"></div>
+</div></li>
+                        <li class="tech-item">PHP <div class="progress">
+  <div class="progress-bar bg-dark" style="width:40%"></div>
+</div></li>
+                        <li class="tech-item">Java <div class="progress">
+  <div class="progress-bar bg-dark" style="width:20%"></div>
+</div></li>
+                        <li class="tech-item">SQL <div class="progress">
+  <div class="progress-bar bg-dark" style="width:40%"></div>
+</div></li>
+                        <li class="tech-item">JavaScript <div class="progress">
+  <div class="progress-bar bg-dark" style="width:40%"></div>
+</div></li>
+                        <li class="tech-item">VueJS <div class="progress">
+  <div class="progress-bar bg-dark" style="width:30%"></div>
+</div></li>
                     </ul>
                 <section>
 
                 </section>
 
         </section>
-        <section class="section">
+        <section class="section project">
                 <header>
                         <h2>Projects</h2>
                         <p>Project sub heading</p>
                     </header>
     
                     <section>
-                        
+                        <div class="row row-edit">
+                            <div class="col "><img class="img" src="images/architech.png" alt="Image"/></div>
+                            <div class="col "><img class="img" src="images/rockband.png" alt="Image"/></div>
+                            <div class="col"><img class="img" src="images/workout.png" alt="Image"/></div>
+                        </div>
                     </section>
         </section>
 
